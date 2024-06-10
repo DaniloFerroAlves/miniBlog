86821dfcfb0367c1f9e4c17f8aaf403f27080913
Danilo <danilaoferraoalves@gmail.com>
2024-06-10 19:12:34 -0300

    Tentando arrumar um bug

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-
-dotenv.config(); 
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
